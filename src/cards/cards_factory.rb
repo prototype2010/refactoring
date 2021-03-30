9192c748c968ebefeae7a4cb5e3f21127cfd011e
prototype2010 <Firstday2010>
2021-03-30 17:53:36 +0300

    factory

--- src/cards/cards_factory.rb
+++ src/cards/cards_factory.rb
@@ -1,42 +1,32 @@
 module CardsFactory
   CARD_TYPES = {
     usual: {
-      name: 'usual',
       start_balance: 50
     },
     capitalist: {
-      name: 'capitalist',
       start_balance: 100
 
     },
     virtual: {
-      name: 'virtual',
       start_balance: 150
     }
   }.freeze
 
   class << self
     def create_card(card_type)
-      card_info = card_init_params(card_type)
+      type_sym = card_type.to_sym
+      card_info = CARD_TYPES[type_sym]
 
-      init_card(card_type, card_info)
-    end
-
-    def init_card(type, card_info)
-      case type
-      when CARD_TYPES[:usual][:name]
+      case type_sym
+      when :usual
         UsualCard.new(UsualCardTax.new, card_info[:start_balance])
-      when CARD_TYPES[:capitalist][:name]
+      when :capitalist
         CapitalistCard.new(CapitalistCardTax.new, card_info[:start_balance])
-      when CARD_TYPES[:virtual][:name]
+      when :virtual
         VirtualCard.new(VirtualCardTax.new, card_info[:start_balance])
       else
         raise WrongCardTypeError
       end
-    end
-
-    def card_init_params(card_type)
-      CARD_TYPES[card_type.to_sym]
     end
   end
 end
